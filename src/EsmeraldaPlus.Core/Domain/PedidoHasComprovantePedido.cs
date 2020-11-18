384462b97d3126c428c587941736684f2124ee47
Julian Torres <julin-2014@outlook.com>
2020-11-17 22:24:51 -0500

    Actualisacion de mapeo

---
+++ src/EsmeraldaPlus.Core/Domain/PedidoHasComprovantePedido.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EsmeraldaPlus.Web
+{
+    public partial class PedidoHasComprovantePedido
+    {
+        public int IdPedido { get; set; }
+        public int IdComprovante { get; set; }
+
+        public virtual ComprovantePedido IdComprovanteNavigation { get; set; }
+        public virtual Pedido IdPedidoNavigation { get; set; }
+    }
+}
